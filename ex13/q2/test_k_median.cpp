74ff5c5799af97cf4f3156b0647f08725241ce86
s1290139 <130520894+s1290139@users.noreply.github.com>
2023-11-20 10:55:57 +0900

    submit Question 2: k-closest elements to the median

---
+++ ex13/q2/test_k_median.cpp
@@ -0,0 +1,25 @@
+#include "k_median.h"
+using namespace std;
+
+int main() {
+    vector<double> data = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0};
+    int k = 2;
+
+    cout << "Data: ";
+    for (const auto &element : data) {
+        cout << element << " ";
+    }
+    cout << endl;
+
+    cout << "Median of data: " << compute_median(data) << endl;
+
+    vector<double> closest = compute_k_closest(data, k);
+
+    cout << "Closest " << k << " elements to the median: ";
+    for (const auto &element : closest) {
+        cout << element << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
